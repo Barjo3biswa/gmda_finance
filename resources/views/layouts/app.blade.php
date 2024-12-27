02a728d39da9a4e282f1e82a09cfa39dc9f32617
Barjo3biswa <biswajit2013laskar@gmail.com>
2024-12-27 10:47:48 +0530

    dfgdfgdfg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,9 @@
 
     @yield('css')
     <style>
-
+        .all-content-wrapper {
+            margin-top: 7rem;
+        }
     </style>
 </head>
 
@@ -48,146 +50,115 @@
         @include('layouts.sidebar')
     </div>
     <div class="all-content-wrapper">
-        {{-- <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="logo-pro">
-                        <a href="#"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
+        <div class="navbar-main-container header-top-area">
+            <div class="top-bar-nav container-fluid">
+                <div class="row">
+                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
+                        <div class="logo-container">
+
+                            <div class="logo-div">
+                                <a href="{{ env('APP_URL') }}/GMDA/gmda-leave/public/"><img class="main-logo"
+                                        src="{{ asset('img/logo/gmda-logo.png') }}" alt=""
+                                        style="max-width: 75px;" /></a>
+                            </div>
+                            <h4>Human Resource Management Information System</h4>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+                        <div class="header-right-info">
+                            <ul class="nav navbar-nav mai-top-nav header-right-menu">
+                                <li class="nav-item">
+                                    <a href="#" data-toggle="dropdown" role="button" aria-expanded="false"
+                                        class="nav-link dropdown-toggle">
+                                        <img src="img/product/pro4.jpg" alt="" />
+                                        <span class="admin-name">{{ Auth::user()->name }}
+                                            (Finance)</span>
+                                        <i class="fa fa-angle-down edu-icon edu-down-arrow"></i>
+                                    </a>
+                                    <ul role="menu"
+                                        class="dropdown-header-top author-log dropdown-menu animated zoomIn">
+                                        <li><a href="#"><span
+                                                    class="edu-icon edu-user-rounded author-log-ic"></span>My
+                                                Profile</a>
+                                        </li>
+                                        @php
+                                            $modules = \App\Models\moduleUrl::where('status', 'Active')->get();
+                                            $logout_url = \App\Models\moduleUrl::where('status', 'Active')
+                                                ->where('id', 1)
+                                                ->first();
+                                        @endphp
+                                        @foreach ($modules as $mod)
+                                            @if (\App\Helpers\commonHelper::isPermissionExist($mod->permission_name))
+                                                @if ($mod->id == 1)
+                                                    <li style="display: flex"><a
+                                                            href="{{ $mod->url }}{{ $mod->project_name }}/login?is_auth=yes{{ $mod->is_jwt_req == 1 ? '&token=' . session('jwt_token') : '' }}"><span
+                                                                class="edu-icon edu-user-rounded author-log-ic"></span>{{ $mod->name }}</a>
+                                                        @if ($mod->id == auth()->user()->landing_module)
+                                                            <a href="javascript:void(0)"><span
+                                                                    class="badge badge-success" style="font-size: 9px">
+                                                                    <i class="fa-solid fa-arrow-right"></i>
+                                                                </span></a>
+                                                        @else
+                                                            <a href="javascript:void(0)" class="def"
+                                                                data-id="{{ $mod->id }}"><span
+                                                                    class="badge badge-secondary"
+                                                                    style="font-size: 8px">Set default</span></a>
+                                                        @endif
+                                                    </li>
+                                                @else
+                                                    <li style="display: flex"><a
+                                                            href="{{ $mod->url }}{{ $mod->project_name }}{{ $mod->is_jwt_req == 1 ? '?token=' . session('jwt_token') : '' }}"><span
+                                                                class="edu-icon edu-user-rounded author-log-ic"></span>{{ $mod->name }}
+                                                        </a>
+                                                        @if ($mod->id == auth()->user()->landing_module)
+                                                            <a href="javascript:void(0)"> <span
+                                                                    class="badge badge-success"
+                                                                    style="font-size: 9px"><i
+                                                                        class="fa-solid fa-check"></i></span></a>
+                                                        @else
+                                                            <a href="javascript:void(0)" class="def"
+                                                                data-id="{{ $mod->id }}"><span
+                                                                    class="badge badge-secondary"
+                                                                    style="font-size: 8px">Set default</span></a>
+                                                        @endif
+
+                                                    </li>
+                                                @endif
+                                            @endif
+                                        @endforeach
+                                        <li>
+                                            <a
+                                                href="{{ $logout_url->url }}{{ $logout_url->project_name }}/logout-ano">
+                                                <span class="edu-icon edu-locked author-log-ic"></span>Log
+                                                Out
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div> --}}
-        <div class="header-advance-area" style="margin-top: 60px;">
-            <div class="header-top-area">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <div class="header-top-wraper">
-                                <div class="row">
-                                    <div class="col-lg-1 col-md-0 col-sm-1 col-xs-12" style="display: none">
-                                        <div class="menu-switcher-pro">
-                                            <button type="button" id="sidebarCollapse"
-                                                class="btn bar-button-pro header-drl-controller-btn btn-info navbar-btn">
-                                                <i class="educate-icon educate-nav"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-
-                                    <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12">
-                                        {{-- <h4 class="title-hrmis">HRMIS</h4> --}}
-                                        <div class="logo-div">
-                                            <a href="{{ env('APP_URL') }}/GMDA/gmda-leave/public/"><img
-                                                    class="main-logo" src="{{ asset('img/logo/gmda-logo.png') }}"
-                                                    alt="" style="max-width: 75px;" /></a>
-                                            <div>
-                                                <p class="title-hrmis">Human</p>
-                                                <p class="title-hrmis">Resource </p>
-                                                <p class="title-hrmis">Management</p>
-                                                <p class="title-hrmis">Information System</p>
-                                            </div>
-                                        </div>
-
-                                    </div>
-
-                                    @include('layouts.topbar')
-
-                                    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                                        <div class="header-right-info">
-                                            <ul class="nav navbar-nav mai-top-nav header-right-menu">
-                                                <li class="nav-item">
-                                                    <a href="#" data-toggle="dropdown" role="button"
-                                                        aria-expanded="false" class="nav-link dropdown-toggle">
-                                                        <img src="img/product/pro4.jpg" alt="" />
-                                                        <span class="admin-name">{{ Auth::user()->name }}
-                                                            (Finance)</span>
-                                                        <i class="fa fa-angle-down edu-icon edu-down-arrow"></i>
-                                                    </a>
-                                                    <ul role="menu"
-                                                        class="dropdown-header-top author-log dropdown-menu animated zoomIn">
-                                                        <li><a href="#"><span
-                                                                    class="edu-icon edu-user-rounded author-log-ic"></span>My
-                                                                Profile</a>
-                                                        </li>
-                                                        @php
-                                                            $modules = \App\Models\moduleUrl::where(
-                                                                'status',
-                                                                'Active',
-                                                            )->get();
-                                                            $logout_url = \App\Models\moduleUrl::where(
-                                                                'status',
-                                                                'Active',
-                                                            )
-                                                                ->where('id', 1)
-                                                                ->first();
-                                                        @endphp
-                                                        @foreach ($modules as $mod)
-                                                            @if (\App\Helpers\commonHelper::isPermissionExist($mod->permission_name))
-                                                                @if ($mod->id == 1)
-                                                                    <li style="display: flex"><a
-                                                                            href="{{ $mod->url }}{{ $mod->project_name }}/login?is_auth=yes{{ $mod->is_jwt_req == 1 ? '&token=' . session('jwt_token') : '' }}"><span
-                                                                                class="edu-icon edu-user-rounded author-log-ic"></span>{{ $mod->name }}</a>
-                                                                        @if ($mod->id == auth()->user()->landing_module)
-                                                                            <a href="javascript:void(0)"><span
-                                                                                    class="badge badge-success"
-                                                                                    style="font-size: 9px">
-                                                                                    <i
-                                                                                        class="fa-solid fa-arrow-right"></i>
-                                                                                </span></a>
-                                                                        @else
-                                                                            <a href="javascript:void(0)"
-                                                                                class="def"
-                                                                                data-id="{{ $mod->id }}"><span
-                                                                                    class="badge badge-secondary"
-                                                                                    style="font-size: 8px">Set
-                                                                                    default</span></a>
-                                                                        @endif
-                                                                    </li>
-                                                                @else
-                                                                    <li style="display: flex"><a
-                                                                            href="{{ $mod->url }}{{ $mod->project_name }}{{ $mod->is_jwt_req == 1 ? '?token=' . session('jwt_token') : '' }}"><span
-                                                                                class="edu-icon edu-user-rounded author-log-ic"></span>{{ $mod->name }}
-                                                                        </a>
-                                                                        @if ($mod->id == auth()->user()->landing_module)
-                                                                            <a href="javascript:void(0)"> <span
-                                                                                    class="badge badge-success"
-                                                                                    style="font-size: 9px"><i
-                                                                                        class="fa-solid fa-check"></i></span></a>
-                                                                        @else
-                                                                            <a href="javascript:void(0)"
-                                                                                class="def"
-                                                                                data-id="{{ $mod->id }}"><span
-                                                                                    class="badge badge-secondary"
-                                                                                    style="font-size: 8px">Set
-                                                                                    default</span></a>
-                                                                        @endif
-
-                                                                    </li>
-                                                                @endif
-                                                            @endif
-                                                        @endforeach
-                                                        <li>
-                                                            <a
-                                                                href="{{ $logout_url->url }}{{ $logout_url->project_name }}/logout-ano">
-                                                                <span
-                                                                    class="edu-icon edu-locked author-log-ic"></span>Log
-                                                                Out
-                                                            </a>
-                                                        </li>
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </div>
+            <div class="header-advance-area" style="margin-top: 0px;">
+                <div class="header-top-area1" style="background: #488acd;">
+                    <div class="container-fluid" style="padding-top: 0; padding-bottom: 0;">
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <div class="header-top-wraper">
+                                    <div class="row">
+                                        @include('layouts.topbar')
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <!-- Mobile Menu start -->
-            @include('layouts.mobile-menu')
-            <!-- Mobile Menu end -->
+                <!-- Mobile Menu start -->
+                @include('layouts.mobile-menu')
+                <!-- Mobile Menu end -->
+            </div>
         </div>
 
         @yield('content')
@@ -340,6 +311,7 @@
             });
         });
     </script>
+
 </body>
 
 </html>
